eaf252a49aa915048cae047bc10e3bd9dc274907
jyan95 <jyan0602@gmail.com>
2019-06-04 14:35:07 -0400

    laid down base auth

--- backend/app/controllers/api/v1/categories_controller.rb
+++ backend/app/controllers/api/v1/categories_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::CategoriesController < ApplicationController
+  skip_before_action :authorized, only: [:index, :show]
+  
+  def index
+    @all = Category.all
+    render json: @all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    render json: @category
+  end
+
   def create
     @category = Category.create(category_params)
   end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
+      resources :categories, only: [:index, :show, :create]
+
       post '/login', to:'auth#create'
       get '/profile', to:'users#profile'
     end
