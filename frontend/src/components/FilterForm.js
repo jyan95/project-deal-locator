262963fd2aa36814d287af960d6c7894892c3a0b
jyan95 <jyan0602@gmail.com>
2019-06-06 11:14:02 -0400

    filter search working

---
+++ frontend/src/components/FilterForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Filter extends React.Component {
+  state = {
+    input: ''
+  };
+
+  handleChange = (e) => {
+    this.setState({input: e.target.value});
+    this.props.onChange(e.target.value);
+  }
+
+  render(){
+    return(
+      <div>
+        <label htmlFor='filter'></label>
+        <input
+          type='text'
+          id='filter'
+          value={this.state.input}
+          onChange={this.handleChange}
+          placeholder='Search by Category'
+        />
+      </div>
+    )
+  }
+}
+
+export default Filter;
