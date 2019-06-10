d71fe01baa9a55835d6be654dd547e2e9dd503c6
jyan95 <jyan0602@gmail.com>
2019-06-10 17:08:58 -0400

    added deal model controller and serializer

---
+++ backend/app/controllers/user_categories_controller.rb
@@ -0,0 +1,19 @@
+class UserCategoriesController < ApplicationController
+  skip_before_action :authorized, only: [:index, :show]
+
+  def index
+    @all = UserCategory.all
+    render json: @all
+  end
+
+  def show
+    @user_category = UserCategory.find(params[:id])
+    render json: @user_category
+  end
+
+  def create
+    # find id of category by slug match (params[:slug])
+    id = Category.find_by(slug:params[:slug]).id
+    UserCategory.create(user_id: params[:user_id], category_id: id)
+  end
+end
