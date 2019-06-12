e3535c275f16964ad9cd1dd8a61d9e07f5aaa19b
jyan95 <jyan0602@gmail.com>
2019-06-12 14:57:24 -0400

    wiring

--- backend/app/controllers/api/v1/user_deals_controller.rb
+++ backend/app/controllers/api/v1/user_deals_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::UserDealsController < ApplicationController
   def create
     # deal = Deal.find_or_create_by(deal_params)
     user_id = decoded_token[0]['user_id']
-    UserDeal.create(user_id: user_id, deal_id:params[:deal_id])
+    Deal.create(frontend_id: params[:deal_id])
+    UserDeal.create(user_id: user_id, deal_id: params[:deal_id])
   end
 
   def index
