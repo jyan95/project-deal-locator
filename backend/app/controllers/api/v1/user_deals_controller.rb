2fa0154f7a083f5de0346bb51c7ca30f9f532fd0
jyan95 <jyan0602@gmail.com>
2019-06-13 11:29:35 -0400

    user can add deals and view them in their profile page

--- backend/app/controllers/api/v1/user_deals_controller.rb
+++ backend/app/controllers/api/v1/user_deals_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UserDealsController < ApplicationController
   def create
-    # deal = Deal.find_or_create_by(deal_params)
     user_id = decoded_token[0]['user_id']
-    Deal.create(frontend_id: params[:deal_id])
-    UserDeal.create(user_id: user_id, deal_id: params[:deal_id])
+    byebug
+    deal = Deal.create(frontend_id: params[:id])
+    UserDeal.create(user_id: user_id, deal_id: deal.id)
   end
 
   def index
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,14 +4,16 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/login', to:'auth#create'
       post '/signup', to: 'users#create'
-      post '/add-deal', to: 'user_deals#create'
+      post '/add-deal/:id', to: 'user_deals#create'
+
       get '/profile', to:'users#profile'
       get '/set-user', to:'users#show'
+      get '/user-deals', to: 'user_deals#index'
 
       resources :users, only: [:index, :update]
       resources :categories, only: [:index, :show]
       resources :user_categories, only: [:create]
-      resources :user_deals, only: [:index, :show, :create, :destroy]
+      resources :user_deals, only: [:show, :destroy]
     end
   end
 end
